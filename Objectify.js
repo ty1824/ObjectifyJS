14b806dd3a37121ef70276573c109d7bfed05463
Tyler Hodgkins <ty1824@gmail.com>
2013-08-09 08:31:00 -0700

    First commit

---
+++ Objectify.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Tyler
+ * Date: 8/8/13
+ * Time: 7:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var Objectify = (function(Kinetic) {
+
+
+
+} (Kinetic));
---
+++ UI.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Tyler
+ * Date: 8/9/13
+ * Time: 12:52 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+Objectify.UI = (function() {
+
+
+
+} ());
